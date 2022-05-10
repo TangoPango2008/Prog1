3e7083d0a072d937bebea462a06f553e310e1d4f
TangoPango2008 <90501471+TangoPango2008@users.noreply.github.com>
2022-05-10 22:53:25 +0200

    Update chapter16.cpp

--- week02-06/chapter16/chapter16.cpp
+++ week02-06/chapter16/chapter16.cpp
@@ -1,17 +1,69 @@
-#include "GUI.h"
-#include "Lines_window.h"
+//g++ drill16_main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o drill16_main `fltk-config --ldflags --use-images`
 
-int main(){
-	try{
-		Lines_window win(Point(100,100), 1000, 800,"lines");
-    return gui_main();
-	}
-	catch(exception& e) {
-    cerr << "Exception: " << e.what() << '\n';
-    return 1;
-}
-    catch (...) {
-    cerr << "Some exception\n";
-return 2;
-}
+#include Simple_window.h
+#include Graph.h
+
+double one(double) { return 1; }
+
+double square(double x) { return xx; }
+
+int main()
+{
+    using namespace Graph_lib;
+
+    int xmax = 1280;
+    int ymax = 720;
+
+    int x_orig = xmax2;
+    int y_orig = ymax2;
+
+    int rmin = -11;
+    int rmax = 11;
+    
+    int n_points = 400;
+
+    Simple_window win {Point{100,100}, xmax, ymax, Canvas};
+
+	Point origo {x_orig, y_orig};
+
+	int xlength = xmax - 40;
+	int ylength = ymax - 40;
+
+	int xscale = 30, yscale = 30;
+
+	Function s (one, rmin, rmax, origo, n_points, xscale, yscale);
+	Function sq (square, rmin, rmax, origo, n_points, xscale, yscale);
+	Function cos_func ( [] (double x) { return cos(x); }, rmin, rmax, origo, n_points, xscale, yscale);
+
+	Axis x {Axisx, Point{20, y_orig}, xlength, xlengthxscale, x};
+	Axis y {Axisy, Point{x_orig, ylength + 20}, ylength, ylengthyscale, y};
+
+	Rectangle r {Point{200,200}, 100, 50};
+
+	r.set_fill_color(Coloryellow);
+	r.set_style(Line_style(Line_styledash, 4));
+
+	Text t {Point{200,400}, Hello graphics!};
+	t.set_font(Fonttimes_bold);
+	t.set_font_size(20);
+
+	Image ii {Point{100,100}, badge.jpg};
+
+	Circle c {Point{700,700}, 100};
+
+	Ellipse e {Point{500,500}, 100, 50};
+	e.set_fill_color(Colorred);
+
+	win.attach(e);
+	win.attach(ii);
+	win.attach(c);
+	win.attach(t);
+	win.attach(r);
+	win.attach(s);
+	win.attach(sq);
+	win.attach(cos_func);
+	win.attach(x);
+	win.attach(y);
+    	win.wait_for_button();
+
 }
