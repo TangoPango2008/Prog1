6920f5b460f774af31d5d985309d9a75f14990c2
TangoPango2008 <90501471+TangoPango2008@users.noreply.github.com>
2022-05-11 10:29:43 +0200

    Update chapter16.cpp

--- week02-06/chapter16/chapter16.cpp
+++ week02-06/chapter16/chapter16.cpp
@@ -1,11 +1,11 @@
-//g++ chapter16.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o ch16 `fltk-config --ldflags --use-images`
+// g++ chapter16.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o ch16 `fltk-config --ldflags --use-images`
 
-#include "Simple_window.h"
+#include "Simple_window.h" 	
 #include "Graph.h"
 
 double one(double) { return 1; }
 
-double square(double x) { return xx; }
+double square(double x) { return x*x; }
 
 int main()
 {
@@ -14,15 +14,15 @@ int main()
     int xmax = 1280;
     int ymax = 720;
 
-    int x_orig = xmax2;
-    int y_orig = ymax2;
+    int x_orig = xmax/2;
+    int y_orig = ymax/2;
 
     int rmin = -11;
     int rmax = 11;
     
     int n_points = 400;
 
-    Simple_window win {Point{100,100}, xmax, ymax, Canvas};
+    Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
 
 	Point origo {x_orig, y_orig};
 
@@ -33,26 +33,27 @@ int main()
 
 	Function s (one, rmin, rmax, origo, n_points, xscale, yscale);
 	Function sq (square, rmin, rmax, origo, n_points, xscale, yscale);
-	Function cos_func ( [] (double x) { return cos(x); }, rmin, rmax, origo, n_points, xscale, yscale);
+	Function cos_func ( [] (double x) { return cos(x); },
+						rmin, rmax, origo, n_points, xscale, yscale);
 
-	Axis x {Axisx, Point{20, y_orig}, xlength, xlengthxscale, x};
-	Axis y {Axisy, Point{x_orig, ylength + 20}, ylength, ylengthyscale, y};
+	Axis x {Axis::x, Point{20, y_orig}, xlength, xlength/xscale, "x"};
+	Axis y {Axis::y, Point{x_orig, ylength + 20}, ylength, ylength/yscale, "y"};
 
 	Rectangle r {Point{200,200}, 100, 50};
 
-	r.set_fill_color(Coloryellow);
-	r.set_style(Line_style(Line_styledash, 4));
+	r.set_fill_color(Color::yellow);
+	r.set_style(Line_style(Line_style::dash, 4));
 
-	Text t {Point{200,400}, Hello graphics!};
-	t.set_font(Fonttimes_bold);
+	Text t {Point{200,400}, "Hello graphics!"};
+	t.set_font(Font::times_bold);
 	t.set_font_size(20);
 
-	Image ii {Point{100,100}, badge.jpg};
+	Image ii {Point{100,100}, "badge.jpg"};
 
 	Circle c {Point{700,700}, 100};
 
 	Ellipse e {Point{500,500}, 100, 50};
-	e.set_fill_color(Colorred);
+	e.set_fill_color(Color::red);
 
 	win.attach(e);
 	win.attach(ii);
@@ -64,6 +65,7 @@ int main()
 	win.attach(cos_func);
 	win.attach(x);
 	win.attach(y);
-    	win.wait_for_button();
+
+    win.wait_for_button();
 
 }
