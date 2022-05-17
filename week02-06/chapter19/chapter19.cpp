1eb3c20fa330e662077caef73cd4bba00ec61497
TangoPango2008 <90501471+TangoPango2008@users.noreply.github.com>
2022-05-17 23:20:35 +0200

    Update chapter19.cpp

--- week02-06/chapter19/chapter19.cpp
+++ week02-06/chapter19/chapter19.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
 using namespace std;
 
 template<typename T>
 struct S{
-	S(T vv = 0) : val{vv} {} //default 0 lesz, maskepp vv
+	S(T vv ) : val{vv} {} 
 
 	T& get();
 	const T& get() const;
 
-	void set(T new_t) {val = new_t;}
-
-	S<T>& operator =(const T& s);
+	S& operator =(const T& vv);
 
 	private: 
 		T val;
-
 };
 
+template<typename T>
+S<T>& S<T>::operator =(const T& s){
+	val = s;
+	return *this; 
+}
+
+template<typename T>
+istream& operator >>(istream& is, vector<T>& v)
+{
+	v.clear();
+	char ch = 0;
+	for(T val; is >> val;)
+	{
+		v.push_back(val);
+		is >> ch;
+		if (ch != ';') break;
+	}
+	return is;
+}
+
+template<typename T>
+void read_val(T& v){
+	std::cin >> v;
+}
 
 template<typename T>
 T& S<T>::get(){
@@ -31,93 +53,58 @@ const T& S<T>::get() const{
 }
 
 template<typename T>
-S<T>& S<T>::operator =(const T& s){
-	val = s;
-	return *this; //ha sikeres akkor az s obj. adja vissza
-}
-
-template<typename T> void read_val(T& v){
-	cin>> v;
-}
-
-template<typename T>
-ostream& operator <<(ostream& os, vector<T>& v){
+ostream& operator<<(ostream& os, vector<T>& v){
 	os << "{ ";
-	for(int i=0; i< v.size(); ++i){
+	for(int i=0; i< v.size(); ++i)
+	{
 		os <<v[i] << (i < v.size()-1 ? ", ": " ");
 	}
 	os << "}\n";
 	return os;
 }
 
-template<typename T>
-istream& operator >>(istream& is, vector<T>& v)
-{
-	char ch = 0;
-	is >> ch;
-	if(ch != '{'){
-		is.unget();
-		return is;
-	}
-	for(T val; is >> val;){
-		v.push_back(val);
-		is >> ch;
-		if (ch != ',') break;
-	}
-	return is;
-}
-
 int main(){
-	S<int> s;
 	S<int> si {37};
 	S<char> sc {'c'};
 	S<double> sd {10.2};
 	S<std::string> ss {"hello"};
-	S<std::vector<int>> svi {std::vector<int>{1,1,2,3,5,8}}; //s tipusu obj ami int-eket tartalmaz
+	S<std::vector<int>> svi {std::vector<int>{1,10,100}};
 
-	cout<< "S<int> : "<< s.get()<< endl
-		<< "S<int> : "<< si.get()<< endl
-		<< "S<char> : "<< sc.get()<< endl
-		<< "S<double> : "<< sd.get()<< endl
-		<< "S<string> : "<< ss.get()<< endl;
+	cout<< "S<int> : "<< si.get()<< endl;
+	cout<< "S<char> : "<< sc.get()<< endl;
+	cout<< "S<double> : "<< sd.get()<< endl;
+	cout<< "S<string> : "<< ss.get()<< endl;
 	  //<< "S<vector> : "<< svi.get()<< endl;
 
-		cout<<"S<vector<int>> : ";
-		for (auto& a: svi.get()){
-				cout<< a <<' ';
-		}
+	cout<<"S<vector<int>> : ";
+	for (auto& a: svi.get())
+	{
+		cout<< a <<' ';
+	}
 		cout<<endl;
 
-		sc.set('s');
+	int ii;
+	cout<<"int:"<<endl;
+	read_val(ii);
+	S<int> ii2 {ii};
 
-		cout<< "S<char> : "<< sc.get() <<endl;
+	char cc;
+	cout<<"char:"<<endl;
+	read_val(cc);
+	S<char> cc2 {cc};
 
-		sd = 3.1416;
+	double dd;
+	cout<<"double:"<<endl;
+	read_val(dd);
+	S<double> dd2 {dd};
 
-		cout << "S<double> : "<< sc.get()<<endl;
+	vector<int> vi;
+	cout<<"vector separated by *space* and end it with \";\""<<endl;
+	read_val(vi);
+	S<vector<int>> vi2 {vi};
 
-		int ii;
-		read_val(ii);
-		S<int> si2 {ii};
-
-		char cc;
-		read_val(cc);
-		S<char> sc2 {cc};
-
-		double dd;
-		read_val(dd);
-		S<double> sd2 {dd};
-
-		cout
-			<<"S<int> : "<<si2.get()<<endl
-			<<"S<char> : "<<sc2.get()<<endl
-			<<"S<double> : "<<sd2.get()<<endl;
-
-			cout<<"S<vector<int>>: (format: {val, val, val})";
-			vector<int> vi2;
-			read_val(vi2);
-			S<vector<int>> svi2 {vi2};
-			cout<<"S<vector<int>> svi2: "<<svi2.get() <<endl;
-
-
+	cout<<"S<int> : "<<ii2.get()<<endl;
+	cout<<"S<char> : "<<cc2.get()<<endl;
+	cout<<"S<double> : "<<dd2.get()<<endl;
+	cout<<"S<vector<int>>: "<<vi2.get() <<endl;
 }
